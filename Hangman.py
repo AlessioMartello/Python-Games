785df4f7eccc45bb4c48d06f6026886087cb2767
Alessio Martello <alessio.martello@hotmail.co.uk>
2020-06-18 10:31:09 +0100

    Beta branch

--- Hangman.py
+++ Hangman.py
@@ -3,6 +3,7 @@ import string
 print("Welcome to this game of hangman! You get 5 attempts to guess the secret word. You can guess just a letter or the whole word in one go, enjoy!")
 # define secret word
 secret_word = "hangman"
+correct_response=list(secret_word)
 #define number of lives
 lives =4
 # Person enters a letter
@@ -18,10 +19,12 @@ guess = ask_question()
 
 while lives >0:
 #check if letter is in the chosen word
-    if guess in secret_word:
+    if guess in correct_response:
         print("Thats in there! Go again!")
+        correct_response = [i for i in correct_response if i != guess]
+        print(correct_response)
         guess=ask_question()
-            #do something
+
     else:
         print("Its not there! You lose a life!")
         print("You have " + str(lives) + " lives left.")
