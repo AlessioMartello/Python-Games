71754900e9310a1caaa5e9dae20a505258a696bc
Alessio Martello <alessio.martello@hotmail.co.uk>
2020-06-18 15:20:48 +0100

    Enhanced outcomes

--- Hangman.py
+++ Hangman.py
@@ -7,30 +7,39 @@ correct_response=list(secret_word)
 #define number of lives
 lives =4
 # Person enters a letter
-def ask_question():
+def ask_question(lives=lives):
     guess = str(input("What letter do you think is in the word? ")).lower()
-    for letter in guess:
-        if guess not in string.ascii_letters:
-            print("Thats not a letter! Try again.")
-            ask_question()
-    return guess
+    if guess==secret_word:
+        lives=0
+        print( "Thats right, you guessed the word! Congrats")
+        return lives, guess
 
-guess = ask_question()
+    elif len(guess) > 1:
+        print("Thats not the full word")
+        return lives, guess
+    else:
+        for letter in guess:
+            if letter not in string.ascii_letters:
+                print("Thats not a letter! Try again.")
+                lives, guess = ask_question()
+        return lives, guess
 
+lives, guess = ask_question()
 while lives >0:
 #check if letter is in the chosen word
+
     if guess in correct_response:
-        print("Thats in there! Go again!")
+        print(str(guess) +"? " + "Thats in there! Go again!")
         correct_response = [i for i in correct_response if i != guess]
         print(correct_response)
-        guess=ask_question()
+        lives, guess = ask_question()
 
     else:
         print("Its not there! You lose a life!")
         print("You have " + str(lives) + " lives left.")
         lives-=1
-        guess=ask_question()
+        lives, guess=ask_question()
         if lives == 0:
-            print("Sorry thats all your guesses! The answer was actaully " + str(secret_word))
+            print("Sorry thats all your guesses! The answer was actually " + str(secret_word))
 
-
+print("Thats it, hope you had fun")
